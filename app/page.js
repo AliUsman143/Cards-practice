849a10218b657a420394b68254deac670795d512
Ali <aliusmanijaz143@gmail.com>
2024-08-31 02:41:46 -0700

    updata home page

--- app/page.js
+++ app/page.js
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const Home  = () => {
+const Home = () => {
   return (
     <div>
       Home page
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos, ad
+        natus praesentium non, consequatur officiis soluta corrupti in nam id
+        est laudantium, eum autem sapiente libero dolore commodi modi!
+      </p>
     </div>
-  )
-}
+  );
+};
 
-export default Home 
+export default Home;
