24d0301883ac25968a5f21be6a4b8e987549be0a
Ali <aliusmanijaz143@gmail.com>
2024-08-31 02:52:01 -0700

    updata home page

--- app/page.js
+++ app/page.js
@@ -9,6 +9,11 @@ const Home = () => {
         natus praesentium non, consequatur officiis soluta corrupti in nam id
         est laudantium, eum autem sapiente libero dolore commodi modi!
       </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, nam
+        eveniet assumenda dignissimos, explicabo eos, dolor dolore quaerat sed
+        iusto labore ad maiores maxime suscipit quia! Quasi placeat saepe illum.
+      </p>
     </div>
   );
 };
