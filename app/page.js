dec5b22e45f4679da256653cdb300d1d524876d0
Ali <aliusmanijaz143@gmail.com>
2024-09-01 22:31:47 -0700

    Add All data from Practice file and run code

--- app/page.js
+++ app/page.js
@@ -1,20 +1,11 @@
 import React from "react";
-import Card11 from "./Component/Card1";
-import AppleFlipCard from "./Component/AppleflipCard";
-
-const Home = () => {
+import WeatherCard from "./APishortcut/WeatherApi";
+const Page = () => {
   return (
     <div>
-      <AppleFlipCard />
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ullam
-        modi facilis autem reprehenderit vero non veritatis, repellat hic
-        temporibus ducimus repellendus maiores qui assumenda quibusdam dicta
-        nulla architecto eius?
-      </p>
-      <h1>My name is ALi </h1>
+  <WeatherCard/>
     </div>
   );
 };
 
-export default Home;
+export default Page;
