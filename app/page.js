75e44cf96cf0ab69b59f8d5db887832e84e9e395
Ali <aliusmanijaz143@gmail.com>
2024-09-01 22:47:29 -0700

    Practice file is totally upload

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import React from "react";
 import WeatherCard from "./APishortcut/WeatherApi";
+import GithubUserSearch from "./Components/GithubApi";
 const Page = () => {
   return (
     <div>
   <WeatherCard/>
+  <GithubUserSearch />
     </div>
   );
 };
