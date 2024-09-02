3c52ce7ea4aae04b5cf554475d958165b1ec460b
Ali <aliusmanijaz143@gmail.com>
2024-09-01 22:00:09 -0700

    My name

--- app/page.js
+++ app/page.js
@@ -3,23 +3,13 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      Home page
       <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsa eos, ad
-        natus praesentium non, consequatur officiis soluta corrupti in nam id
-        est laudantium, eum autem sapiente libero dolore commodi modi!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam ullam
+        modi facilis autem reprehenderit vero non veritatis, repellat hic
+        temporibus ducimus repellendus maiores qui assumenda quibusdam dicta
+        nulla architecto eius?
       </p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, nam
-        eveniet assumenda dignissimos, explicabo eos, dolor dolore quaerat sed
-        iusto labore ad maiores maxime suscipit quia! Quasi placeat saepe illum.
-      </p>
-      <h1>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora
-        expedita inventore consequatur necessitatibus adipisci. At delectus
-        doloribus animi. Iure eveniet hic cumque quia saepe repellendus
-        perspiciatis culpa dicta. Consequuntur, ipsa.
-      </h1>
+      <h1>My name is ALi </h1>
     </div>
   );
 };
